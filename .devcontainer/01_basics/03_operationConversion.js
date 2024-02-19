53b333aaf7d10052ae7e5597b34b4bfa6adf33ee
jasneet-11 <97148151+jasneet-11@users.noreply.github.com>
2024-02-19 19:23:25 +0000

    Postfix and prefix operations performed

--- .devcontainer/01_basics/03_operationConversion.js
+++ .devcontainer/01_basics/03_operationConversion.js
@@ -38,3 +38,77 @@ let stringNumber= String(somenumber)
 console.log(somenumber);
 console.log(typeof somenumber); // this will give 54 as answer bcoz somenumber has value stored 54 that is number
 console.log(typeof stringNumber); // we see that number is converted in string.
+
+
+//***********************************************   OPERATIONS       ************************************************* 
+/*
+let value = 7
+let negValue = -value
+console.log(negValue);
+
+console.log(3+3);//6
+console.log(3*3);//9
+console.log(3-3);//0
+console.log(3**3); //** indicates raised to power output will be 27
+console.log(2/3);
+console.log(2%3);
+*/ 
+
+let str1 = "Jasneet"
+let str2 = "Singh"
+
+let str3 = str1+ str2
+console.log(str3); 
+/*
+console.log("1"+2); //12
+console.log(1+"2"); //12
+Here both the operations are giving output as 12 so we need 
+to check whats happening here actually. Both of these are complex situations
+console.log("1"+2+2); //122
+console.log(1+2+"2"); //32
+*/
+/*
+now we have to check why such answers are occuring like 122 and 32 why 1+2 happened in the fourth code? 
+ANS:-String first=>all treated as string
+if operations first and string last then first operations will happen and then string will get attached 
+more detailed answer is in this link:- https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
+read about abstract operations to get more details
+Use parenthesis to get more clarity on which operation to perform first and thats preferred in the industry
+*/ 
+console.log(+true);// it gives output as 1 and,
+//console.log(true+); this is not possible
+console.log(+"");// we know empty parenthesis give value=0
+
+//let num1,num2,num3
+
+//num1 = num2 = num3 = 2+2       such code dont improve the readability of the code and are not preferred.
+
+let gameCounter = 100
+gameCounter++; //here ++gameCounter also showing up 101 as value but it is prefix increment
+console.log(gameCounter);// value increased by 101 output of postfix increment.
+
+//Study about postfix and prefix operations from mdn.0
+
+//The link to the document is: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment
+/*
+POSTFIX INCREMENT : If used postfix, with operator after operand (for example, x++), 
+the increment operator increments and returns the value before incrementing.
+*/
+let x = 3;
+const y = x++;   // x is 4; y is 3
+console.log([x,y]);
+let x2 = 9n;
+const y2 = x2++; // x2 is 10n; y2 is 9n
+console.log([x2,y2]);
+/*
+PREFIX INCREMENT : If used prefix, with operator before operand (for example, ++x), 
+the increment operator increments and returns the value after incrementing.
+*/
+let x = 10;
+const y = ++x; // x is 11; y is 11
+console.log([x,y]);
+let x2 = 3n;
+const y2 = ++x2; // x2 is 4n; y2 is 4n
+console.log([x2,y2]);
+
+//NOTE:- ++(++x); SyntaxError: Invalid left-hand side expression in prefix operation.
