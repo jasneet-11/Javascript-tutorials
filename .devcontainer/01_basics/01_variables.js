79ede872c53601d9640b7ce6a6d4126d2e3861ea
Jasneet Singh Arora <97148151+jasneet-11@users.noreply.github.com>
2024-08-05 10:16:44 +0000

    necessary changes

--- .devcontainer/01_basics/01_variables.js
+++ .devcontainer/01_basics/01_variables.js
@@ -1,15 +1,16 @@
 const accountId =144553
-let accountEmail="jasneetarora@gmail.com"
+let accountEmail="jasneetarora@google.com"
 var accountPassword="1234567"
-/* prefer not to use var 
+/* prefer not to use var, stick to let and const. Var was used in old codes
+
 because of issue in functional and block scope
 */ 
-accountCity="Jodhpur" // this is also possible but it is not a good way to do the things.
+accountCountry="France" // this is also possible but it is not a good way to do the things.
 let accountState;  // javascript will give undefined value because it has no value assocoiated with it.
 accountEmail="jasprretbagga@gmail.com"
 accountPassword="110695"
 accountCity="Paris"
 
  // accountId=2 // not allowed changing a const value
-console.table([accountEmail,accountId,accountPassword,accountCity,accountState]) // gives the output in form of table
+console.table([accountEmail,accountId,accountPassword,accountCountry,accountState]) // gives the output in form of table
 //console.log(accountId);
